7dac4f9b1238a42fa0da3a0baa3f0e41e4899089
Natalia Semenova <natnatanatasha@gmail.com>
2025-11-25 14:08:35 +0100

    add files

---
+++ vite.config.cjs
@@ -0,0 +1,25 @@
+const { defineConfig } = require("vite");
+const { viteStaticCopy } = require("vite-plugin-static-copy");
+const path = require("path");
+
+module.exports = defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        { src: "header.html", dest: "" },
+        { src: "content.html", dest: "" },
+        { src: "footer.html", dest: "" },
+        { src: "img/*", dest: "img/" },
+        { src: "css/*", dest: "css/" },
+      ],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        contentDetails: path.resolve(__dirname, "contentDetails.html"),
+      },
+    },
+  },
+});
